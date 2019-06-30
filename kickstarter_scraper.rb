6a009eccfcbcb2715145c5276c20a471af83b4c6
Mark May <mamark90@gmail.com>
2019-06-30 14:45:09 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,63 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'pry'
+require 'awesome_print'
 
-def create_project_hash
-  # write your code here
-end
+  def create_project_hash
+    html = File.read('fixtures/kickstarter.html')
+    kickstarter = Nokogiri::HTML(html)
+  end
+  require 'nokogiri'
+  require 'pry'
+  require 'awesome_print'
+
+  #class Scraper
+
+    def create_project_hash
+      html = File.read('fixtures/kickstarter.html')
+      kickstarter = Nokogiri::HTML(html)
+      binding.pry
+    end
+
+  create_project_hash
+  require 'nokogiri'
+  require 'pry'
+  require 'awesome_print'
+
+  #class Scraper
+
+    def create_project_hash
+      html = File.read('fixtures/kickstarter.html')
+      kickstarter = Nokogiri::HTML(html)
+      binding.pry
+    end
+
+  create_project_hash
+  require 'nokogiri'
+  require 'pry'
+  require 'awesome_print'
+
+  #class Scraper
+
+    def create_project_hash
+      html = File.read('fixtures/kickstarter.html')
+      kickstarter = Nokogiri::HTML(html)
+    end
+
+  create_project_hash
+  require 'nokogiri'
+  require 'pry'
+  require 'awesome_print'
+
+    def create_project_hash
+      html = File.read('fixtures/kickstarter.html')
+      kickstarter = Nokogiri::HTML(html)
+
+      # projects: kickstarter.css("li.project.grid_4")
+      # title project.css("h2.bbcard_name strong a").text
+
+
+    end
+
+    binding.pry
+
+  create_project_hash
